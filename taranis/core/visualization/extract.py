f7f7078b3c18fbe1f124c0c8d02d667c04630ca5
Setepenre <pierre.delaunay@outlook.com>
2023-09-08 09:09:08 -0400

    -

--- taranis/core/visualization/extract.py
+++ taranis/core/visualization/extract.py
@@ -4,21 +4,29 @@ import torch.nn.functional as F
 
 
 
-def generate_image(model, input_shape, label, lr, confidence_target=0.99):
-
+def generate_image(model, input_shape, label, lr=100, confidence_target=0.99):
+    """Extract the internal representation of a label"""
     # Disable model gradient
     model.eval()
     for param in model.parameters():
         param.require_grads = False
 
+    return tweak_image(model, torch.zeros(1, *input_shape), label, lr, confidence_target)
 
-    generated_image = torch.zeros(1, *input_shape).cuda().requires_grad_(True)
+
+def tweak_image(model, original, label, lr=1, confidence_target=0.99, max_iter=1000):
+    # Disable model gradient
+    model.eval()
+    for param in model.parameters():
+        param.require_grads = False
+
+    generated_image = original.cuda().requires_grad_(True)
     assert generated_image.is_leaf
 
     optimizer = optim.SGD([generated_image], lr=lr)
     confidence = 0
 
-    while confidence < confidence_target:
+    for _ in range(max_iter):
         optimizer.zero_grad()
         output = model(generated_image)
         loss = F.cross_entropy(output, torch.tensor([label], dtype=torch.long).cuda())
@@ -26,4 +34,16 @@ def generate_image(model, input_shape, label, lr, confidence_target=0.99):
         optimizer.step()
         confidence = output[0, label]
 
-    return generated_image.detach().cpu()[0], confidence
+        if confidence > confidence_target:
+            break
+
+    img = generated_image.detach().cpu()
+    return img[0], confidence.item()
+
+
+def renormalize(data, min, max):
+    mn = data.min()
+    mx = data.max()
+    rg = mx - mn
+    normalized = (data - mn) / rg
+    return normalized * (max - min) + min
